d777a12bb918b24932f75ca3baa070b44ac870d5
natgho <admin@sezerbozkir.com>
2019-12-01 18:48:10 +0300

    second weeks changed added.

---
+++ 2_hafta/degisken_tipleri_ve_operatorler.py
@@ -0,0 +1,25 @@
+# Veri Tipleri
+sayi_1 = 1996
+gunumuz = 2019
+kayar_noktali_sayi = 18.9976
+metin_degerim = "Emre YILDIRIM"
+# print(sayi_1,
+#      kayar_noktali_sayi, metin_degerim)
+
+sonuc = gunumuz - sayi_1
+print("İlk Sonuc:", sonuc)
+sonuc = 10%2
+print("İkinci Sonuc:", sonuc)
+sonuc = """Bu gün Ahmet'e g" ittim."""
+sonuc = 10
+# print("Ucuncu Sonuc:" +  sonuc)
+
+# + - * ** / += -= /= *=
+sayi_1 = 10
+sayi_1 += 10
+sayi_1 = sayi_1 + 10
+print("*" * 50)
+print("Ne kadar sekilli bir yazi")
+print("*" * 50)
+
+
---
+++ 2_hafta/yazdirma_sekillleri.py
@@ -0,0 +1,5 @@
+dogum_tarihi = 1881
+isim_soyisim = "Mustafa Kemal Ataturk"
+# print(dogum_tarihi, isim_soyisim, sep="---", end=" Kurucu")
+print("Isım Soyisim: {}\nDogum Tarihi:{}".format(isim_soyisim, dogum_tarihi))
+print(f"İsim soyisim:{isim_soyisim}\nDogum Tarihi: {1880 + 1}")
